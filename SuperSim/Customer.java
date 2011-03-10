548249fe808ec15f796892b8c9acd1c9fac9ec7a
Alexander Stuart-Kregor <easkay@me.com>
2011-03-10 22:32:19 +0000

    Backup

--- SuperSim/Customer.java
+++ SuperSim/Customer.java
@@ -11,12 +11,12 @@ public class Customer
 {
     private final String CUSTOMER_TYPE; 
     private final int ID;
-    private final int  MIN_ITEMS = 1; 
-    private final int MAX_ITEMS;
-    private final int ITEMS_TO_PICK; //Range of items to pick dependent on type of Customer.
+    private final int  MIN_ITEMS = 1; //going to change soon
+    private final int MEAN_ITEMS;
+    private final long ITEMS_TO_PICK; //Range of items to pick dependent on type of Customer.
     private int TIME_PER_ITEM = 5; //Arbitrary
     private final int TOTAL_ITEMS_AVAIL;
-    private int shoppingTime; //The time the customer spends picking items
+    private long shoppingTime; //The time the customer spends picking items
     private ArrayList<Item> trolley;
     private Random rand;
     private Math math;
@@ -40,43 +40,42 @@ public class Customer
          * Might have to change from switch to if and ifelse or have if and else if calculate the int for
          * switch based omc probabilities and distributions and then have a rand.nextInt() as last else.
          */
-        double multiplyer = math.abs(rand.nextGaussian()*2*14); //Probability * Mean * StdDev then absolute.
+        //double multiplyer = math.abs(rand.nextGaussian()*2*14); //Probability * Mean * StdDev then absolute.
         switch(rand.nextInt(5)){
-            case 0: MAX_ITEMS = 5; CUSTOMER_TYPE = "Business"; break;
-            case 1: MAX_ITEMS = 15; CUSTOMER_TYPE = "Old"; break;
-            case 2: MAX_ITEMS = 90; CUSTOMER_TYPE = "Family"; break;
-            case 3: MAX_ITEMS = 3; CUSTOMER_TYPE = "Children"; break;
-            case 4: MAX_ITEMS = 25; CUSTOMER_TYPE = "Generic"; break;
-            default: MAX_ITEMS = MIN_ITEMS; CUSTOMER_TYPE = "Generic"; break;//Ensures the compiler knows that it can always set MAX_ITEMS
+            case 0: MEAN_ITEMS = 5; CUSTOMER_TYPE = "Business"; break;
+            case 1: MEAN_ITEMS = 15; CUSTOMER_TYPE = "Old"; break;
+            case 2: MEAN_ITEMS = 90; CUSTOMER_TYPE = "Family"; break;
+            case 3: MEAN_ITEMS = 3; CUSTOMER_TYPE = "Children"; break;
+            case 4: MEAN_ITEMS = 25; CUSTOMER_TYPE = "Generic"; break;
+            default: MEAN_ITEMS = MIN_ITEMS; CUSTOMER_TYPE = "Generic"; break;//Ensures the compiler knows that it can always set MEAN_ITEMS
         }
-        int itemPickLimit = TOTAL_ITEMS_AVAIL;
-        while(itemPickLimit >= TOTAL_ITEMS_AVAIL){//Makes sure the Customer can't choose more items than exist.
-            itemPickLimit = MIN_ITEMS + rand.nextInt(MAX_ITEMS - MIN_ITEMS); //Finds range to pick items.
-        }
+        long itemPickLimit;// = TOTAL_ITEMS_AVAIL;
+        do {//Makes sure the Customer can't choose more items than exist.
+            itemPickLimit = MIN_ITEMS + math.abs(math.round(rand.nextGaussian())+MEAN_ITEMS); //Finds range to pick items.
+        }while(itemPickLimit >= TOTAL_ITEMS_AVAIL);
         ITEMS_TO_PICK = itemPickLimit;
         setShoppingTime(ITEMS_TO_PICK * TIME_PER_ITEM);
     }    
 
     public double addItem()
     {
-        double itemPrice = 0;
-        if(shoppingTime % (rand.nextInt(shoppingTime)+1) == 0){ //If timeinstore % timeperitem == 0. Might do by ticks so shoppingTime % ticks == 0
-            if(shoppingTime % (rand.nextInt(TIME_PER_ITEM)+1) == 0){ //If timeinstore % timeperitem == 0. Might do by ticks so shoppingTime % ticks == 0
-                if(trolley.size() < ITEMS_TO_PICK){
-                    Item itemSelect = productList.get(rand.nextInt(TOTAL_ITEMS_AVAIL));
-                    trolley.add(itemSelect);
-                    shoppingTime-= (TIME_PER_ITEM);
-                    itemPrice += itemSelect.getPrice();
-                }
-                else
-                {
-                    //Customer has all required items
-                    setShoppingTime(0);
-                }
+        double itemPrice = 0.0;
+        if((shoppingTime % (rand.nextInt(TIME_PER_ITEM)+1)) == 0){ //If timeinstore % timeperitem == 0. Might do by ticks so shoppingTime % ticks == 0
+            if(trolley.size() < ITEMS_TO_PICK){
+                Item itemSelect = productList.get(rand.nextInt(TOTAL_ITEMS_AVAIL));
+                trolley.add(itemSelect);
+                shoppingTime-= (TIME_PER_ITEM);
+                itemPrice += itemSelect.getPrice();
+                System.out.println("Item pickup successful, \nshoppingTime = " + shoppingTime + "\nItems = " + trolley.size() + "\nItems to pick = " + ITEMS_TO_PICK);
+            }
+            else
+            {
+                //Customer has all required items
+                setShoppingTime(0);
             }
         }
-        timeInStore++;
-        
+        timeInStore++;   
+        System.out.println("METHOD CALL");
         return itemPrice;
     }
 
@@ -93,8 +92,9 @@ public class Customer
      * An accessor method for use in the Store class
      * @return SHOPPING_TIME
      */
-    public int getShoppingTime()
+    public long getShoppingTime()
     {
+        System.out.println("Shopping Time = " + shoppingTime);
         return shoppingTime;
     }
 
@@ -102,7 +102,7 @@ public class Customer
      * A mutator method to decrement the time remaining to pick items based on the global time from the Store class
      * @param newTime the new global time
      */
-    public void setShoppingTime(int newTime)
+    public void setShoppingTime(long newTime)
     {
         shoppingTime = newTime;
     }
