60046e3a52f9bbf9058ad79166135beb22b483e4
NeoNMD <neonmd@hotmail.co.uk>
2011-03-10 05:42:27 +0000

    Doccumented the Checkout class.

--- SuperSim/Checkout.java
+++ SuperSim/Checkout.java
@@ -9,7 +9,6 @@ public class Checkout
 {
     private ArrayList<Customer> queue;
     private ArrayList<String> itemReceipt;
-    //private ArrayList<String> valueReceipt; //can fetch from itemlist.
     private boolean express = false;
     private Customer currentCustomer;
     private final int ITEM_SCAN_SPEED;
@@ -18,17 +17,20 @@ public class Checkout
     private int totalQueue;
     /**
      * Constructor for objects of class Checkout
+     * @param isExpress Toggles if checkout to be created is express or not.
      */
     public Checkout(boolean isExpress)
     {
         express = isExpress;
         ArrayList itemReceipt = new ArrayList<String>();
-        //ArrayList valueReceipt = new ArrayList<String>();
         queue = new ArrayList<Customer>();
         ITEM_SCAN_SPEED = 3;
         scanInterval = 3;
     }
 
+    /**
+     * Runs once per tick, controls one customer at the till and all customers in the queue.
+     */
     public void run()
     {
         if(queueHasCustomer())
@@ -57,7 +59,6 @@ public class Checkout
             {
                 makeReceipt();
                 currentCustomer.getTimeInQueue();
-                saveStats();
                 currentCustomer = null;
             }
         }
@@ -72,11 +73,18 @@ public class Checkout
         }
     }
     
+    /**
+     * Sums up all the time customers spend in the queue.
+     */
     public void calcAverageQueue(Customer cust)
     {
         totalQueue += cust.getTimeInQueue();
     }
     
+    /**
+     * Calculates the average time a customer spends in the queue.
+     * @return Returns the average length as double.
+     */
     public double getAverageQueue()
     {
         double average = totalQueue/customerCounter;
@@ -88,11 +96,10 @@ public class Checkout
         //what does this entail?
     }
 
-    private void saveStats()
-    {
-        //stub
-    }
-
+    /**
+     * Returns true if the queue is not empty.
+     * @return True if queue has at lest 1 customer.
+     */
     private boolean queueHasCustomer()
     {
         if (queue.size() > 0)
@@ -105,6 +112,10 @@ public class Checkout
         }
     }
 
+    /**
+     * Returns true if there is a customer at the till.
+     * @return True if the till has a customer. 
+     */
     private boolean hasCustomer()
     {
         if (!(currentCustomer == null))
@@ -116,12 +127,20 @@ public class Checkout
             return false;
         }
     }
-
+    
+    /**
+     * Returns the queue size
+     * @return returns queue size as an integer.
+     */
     public int getQueueLength()
     {
         return queue.size();
     }
 
+    /**
+     * Checks if the customer at the till has any more items in the trolley.
+     * @return True if the customer has items in the trolley.
+     */
     private boolean hasItems()
     {
         if (currentCustomer.getTrolley().size() > 0)
@@ -139,6 +158,9 @@ public class Checkout
         return 0; //arbitrary//do something with a really small chance of delay, and the delay can be between a small range.
     }
 
+    /**
+     * Scan items takes an item out of the trolley of the customer and adds the item name to the reciept list.
+     */
     private void scanItems()
     {
         Item thisItem = currentCustomer.removeTrolleyItem();
@@ -146,6 +168,10 @@ public class Checkout
         //add price?
     }
 
+    /**
+     * Method to get the number of Items in all the trolleys in the queue.
+     * @return Number of items in the queue as an int.
+     */
     public int itemCount()
     {
         int items=0;
@@ -156,11 +182,17 @@ public class Checkout
         return items;
     }
 
+    /**
+     * Returns if the checkout is an express checkout.
+     */
     public boolean isExpress()
     {
         return express;
     }
 
+    /**
+     * Method to add a customer to the queue of the checkout.
+     */
     public void add(Customer newCustomer)
     {
         queue.add(newCustomer);
