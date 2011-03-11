f7d8a93273f473530a72437f15bada325a54f1ae
Alexander Stuart-Kregor <easkay@me.com>
2011-03-11 01:34:46 +0000

    Put biasing probability framework in place for customer, values are currently arbitrary.

--- SuperSim/Customer.java
+++ SuperSim/Customer.java
@@ -11,11 +11,12 @@ public class Customer
 {
     private final String CUSTOMER_TYPE; 
     private final int ID;
-    private final int  MIN_ITEMS = 1; //going to change soon
+    //private final int  MIN_ITEMS = 1; //going to change soon
     private final int MEAN_ITEMS;
     private final long ITEMS_TO_PICK; //Range of items to pick dependent on type of Customer.
     private int TIME_PER_ITEM = 5; //Arbitrary
     private final int TOTAL_ITEMS_AVAIL;
+    private final double BUSINESS_PROB, OLD_PROB, CHILD_PROB, GENERIC_PROB;
     private long shoppingTime; //The time the customer spends picking items
     private ArrayList<Item> trolley;
     private Random rand;
@@ -36,22 +37,87 @@ public class Customer
         trolley = new ArrayList<Item>();
         rand = new Random();
         TOTAL_ITEMS_AVAIL = productList.size();
-        /*Code to bias random based on time
-         * Might have to change from switch to if and ifelse or have if and else if calculate the int for
-         * switch based omc probabilities and distributions and then have a rand.nextInt() as last else.
-         */
-        //double multiplyer = math.abs(rand.nextGaussian()*2*14); //Probability * Mean * StdDev then absolute.
-        switch(rand.nextInt(5)){
-            case 0: MEAN_ITEMS = 5; CUSTOMER_TYPE = "Business"; break;
-            case 1: MEAN_ITEMS = 15; CUSTOMER_TYPE = "Old"; break;case 2: MEAN_ITEMS = 12; CUSTOMER_TYPE = "Family"; break;
-            case 3: MEAN_ITEMS = 3; CUSTOMER_TYPE = "Children"; break;
-            case 4: MEAN_ITEMS = 18; CUSTOMER_TYPE = "Generic"; break;
-            default: MEAN_ITEMS = MIN_ITEMS; CUSTOMER_TYPE = "Generic"; break;//Ensures the compiler knows that it can always set MEAN_ITEMS
+        if(hour >= 0 && hour < 8) //Midnight to 8am i.e. slow time
+        {
+            BUSINESS_PROB = 0.2;
+            OLD_PROB = 0.3;
+            CHILD_PROB = 0.6;
+            GENERIC_PROB = 1.0;
+        }
+        else if(hour >= 8 && hour < 10)
+        {
+            BUSINESS_PROB = 0.01;
+            OLD_PROB = 0.3;
+            CHILD_PROB = 0.7;
+            GENERIC_PROB = 1.0;
+        }
+        else if(hour >= 10 && hour < 12)
+        {
+            BUSINESS_PROB = 0.1;
+            OLD_PROB = 0.5;
+            CHILD_PROB = 0.6;
+            GENERIC_PROB = 1.0;
+        }
+        else if(hour >= 12 && hour < 14)
+        {
+            BUSINESS_PROB = 0.4;
+            OLD_PROB = 0.6;
+            CHILD_PROB = 0.9;
+            GENERIC_PROB = 1.0;
+        }
+        else if(hour >= 14 && hour < 17)
+        {
+            BUSINESS_PROB = 0.25;
+            OLD_PROB = 0.3;
+            CHILD_PROB = 0.7;
+            GENERIC_PROB = 1.0;
+        }
+        else if(hour >= 17 && hour < 19)
+        {
+            BUSINESS_PROB = 0.075;
+            OLD_PROB = 0.1;
+            CHILD_PROB = 0.2;
+            GENERIC_PROB = 1.0;
+        }
+        else if(hour >= 19 && hour < 22)
+        {
+            BUSINESS_PROB = 0.09;
+            OLD_PROB = 0.1;
+            CHILD_PROB = 0.4;
+            GENERIC_PROB = 1.0;
+        }
+        else
+        {
+            BUSINESS_PROB = 0.2;
+            OLD_PROB = 0.3;
+            CHILD_PROB = 0.6;
+            GENERIC_PROB = 1.0;
+        }
+        double decider = rand.nextDouble();
+        if(decider >= 0 && decider <=BUSINESS_PROB)
+        {
+            MEAN_ITEMS = 5;
+            CUSTOMER_TYPE = "Business";
+        }
+        else if(decider > BUSINESS_PROB && decider <= OLD_PROB)
+        {
+            MEAN_ITEMS = 15;
+            CUSTOMER_TYPE = "Old";
+        }
+        else if(decider > OLD_PROB && decider <= CHILD_PROB)
+        {
+            MEAN_ITEMS = 3;
+            CUSTOMER_TYPE = "Children";
+        }
+        else //assume generic
+        {
+            MEAN_ITEMS = 18;
+            CUSTOMER_TYPE = "Generic";
         }
         long itemPickLimit;// = TOTAL_ITEMS_AVAIL;
         do {//Makes sure the Customer can't choose more items than exist.
-            itemPickLimit = MIN_ITEMS + math.abs(math.round(rand.nextGaussian())+MEAN_ITEMS); //Finds range to pick items.
-        }while(itemPickLimit >= TOTAL_ITEMS_AVAIL);
+            itemPickLimit = math.abs(math.round(rand.nextGaussian())+MEAN_ITEMS); //Finds range to pick items.
+        }while(itemPickLimit >= TOTAL_ITEMS_AVAIL || itemPickLimit <= 0);
         ITEMS_TO_PICK = itemPickLimit;
         setShoppingTime(ITEMS_TO_PICK * TIME_PER_ITEM);
     }    
@@ -89,7 +155,6 @@ public class Customer
      */
     public long getShoppingTime()
     {
-        System.out.println("Shopping Time = " + shoppingTime);
         return shoppingTime;
     }
 
